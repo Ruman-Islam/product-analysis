2fa0e29a5a4df98d00c1ef0b4419e91350148963
Ruman-Islam <rumanislam48@gmail.com>
2022-04-05 08:11:31 +0600

    footer background changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebook, faTwitter, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import './Footer.css';
 
 const Footer = () => {
@@ -7,6 +9,12 @@ const Footer = () => {
             <h1>CAMERA WORLD</h1>
             <p>Copyright &copy;2022 CAMERA WORLD</p>
             <small>All rights reserved</small><br />
+            <div className='social-icon-container'>
+                <FontAwesomeIcon className='icon' icon={faFacebook} />
+                <FontAwesomeIcon className='icon' icon={faTwitter} />
+                <FontAwesomeIcon className='icon' icon={faGithub} />
+                <FontAwesomeIcon className='icon' icon={faLinkedin} />
+            </div>
         </footer>
     );
 };
